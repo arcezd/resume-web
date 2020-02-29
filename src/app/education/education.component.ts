c8798a9916621d78e9f032a80adadcac80060ebf
Diego Arce <Diego@arce.cr>
2020-02-29 09:43:16 -0600

    Implement UserProfile at Education component
    Signed-off-by: Diego Arce <Diego@arce.cr>

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
+
+import { ProfileService } from '../profile/profile.service';
+import { UserProfile } from '../profile/user-profile';
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
@@ -7,9 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  public profile$: Observable<UserProfile>;
+
+  constructor(
+    public profileService: ProfileService
+  ) { }
 
   ngOnInit() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.profile$ = this.profileService.GetUserProfile().pipe(share());
   }
 
 }
